823cd5bc633f97e239a17cc0fa2b20ef2a795dc8
pater97 <giuseppepater997@gmail.com>
2022-10-28 17:11:01 +0200

    fixed navbar | to do navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,76 +1,106 @@
-import React from 'react'
-import { useState , useEffect} from 'react'
+import React, { Component } from 'react'
+// import { useState , useEffect} from 'react'
+import '../styles/navbar.scss'
 
-function Navbar() {
-    // stati
-    const [dropdow, setDrop] = useState(false)
-    const [scroll, setScroll] = useState(false)
-    // dropdown
-    function drop() {
-        setDrop(true)
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            dropDown: false,
+            scroll: false
+        }
     }
-    function noDrop() {
-        setDrop(false)
-        console.log('nodrop')
+    // dropdown
+    drop = () => {
+        this.setState({ dropDown: true })
+    }
+    noDrop = () => {
+        this.setState({ dropDown: false })
     }
 
     // effetto allo scroll
-    function handleScroll() {
+    handleScroll = () => {
         let windowScroll = window.scrollY;
         let scrolly = false;
         if (windowScroll > 110) {
             scrolly = true;
         }
-        setScroll(scrolly)
-        console.log(scroll)
+        this.setState({ scroll: scrolly })
     }
-    //   metto in ascolto
-    useEffect(() => {
-
-        window.addEventListener("scroll", handleScroll);
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
         return () => {
-            window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("scroll", this.handleScroll);
         };
-    }, [scroll]);
+    }
 
-    return (
-        <div id='header' className={scroll ? 'activeHide' : ''}>
-            <div className='banda'></div>
-            <nav id='nav'>
-                <div className='Home'>
-                    <a href='#'>
-                        HOME
-                    </a>
-                </div>
-                <div className='brice'>
-                    <a href='#' onFocus={drop} onBlur={noDrop}>
-                        B®ICE-STUDIO
-                    </a>
-                    <div className='dropdown'>
-                        <ul className={`dropdown ${dropdow ? 'activeDrop' : ''}`}>
-                            <li>
-                                FOTO
-                            </li>
-                            <li>
-                                SERVIZI
-                            </li>
-                            <li>
-                                ASCOLTA
-                            </li>
-                            <li>
-                                SETUP
-                            </li>
-                        </ul>
+
+    render() {
+
+        return (
+            <div id='header' className={this.state.scroll ? 'activeHide' : ''}>
+                <div className='banda'></div>
+                <nav id='nav'>
+                    <div className='Home'>
+                        <button>
+                            <h4>
+                                HOME
+                            </h4>
+                        </button>
                     </div>
-                </div>
-                <div className='contatti'>
-                    <a href='#'>
-                        CONTATTI
-                    </a>
-                </div>
-            </nav>
-        </div>
-    )
+                    <div className='brice'>
+                        <button onFocus={this.drop} onBlur={this.noDrop}>
+                            <h4>
+                                B®ICE-STUDIO
+                            </h4>
+                        </button>
+                        <div className='dropdown'>
+                            <ul className={`dropdown ${this.state.dropDown ? 'activeDrop' : ''}`}>
+                                <li>
+                                    <button>
+                                        <h4>
+                                            FOTO
+                                        </h4>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button>
+                                        <h4>
+
+                                            SERVIZI
+                                        </h4>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button>
+                                        <h4>
+
+                                            ASCOLTA
+                                        </h4>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button>
+                                        <h4>
+                                            SETUP
+                                        </h4>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='contatti'>
+                        <button>
+                            <h4>
+                                CONTATTI
+                            </h4>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default Navbar
